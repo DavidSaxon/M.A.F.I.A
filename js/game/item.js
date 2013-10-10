7e78ac372e529bfdd67fba38b0f4ba67733f5bef
jon0 <jono4728@gmail.com>
2013-10-10 20:24:25 +1300

    refactor loading objects

--- js/game/item.js
+++ js/game/item.js
@@ -4,6 +4,8 @@ function windmill(x, y, z) {
 	this.typeName = "windmill";
 	this.basesize = 30.0;
 	this.position = new THREE.Vector3(x, y, z);
+	this.yRot = 0.0;
+	this.scalesize = 40;
 	this.bladeSpeed = Math.random() * 0.5;
 }
 
@@ -12,12 +14,14 @@ function house(x, y, z, yrot) {
 	this.basesize = 70.0;
 	this.position = new THREE.Vector3(x, y, z);
 	this.yRot = yrot;
+	this.scalesize = 20;
 }
 
 function dude(x, y, z, size) {
 	this.typeName = "dude";
 	this.basesize = 15.0;
 	this.position = new THREE.Vector3(x, y, z);
+	this.yRot = 0.0;
 	this.scalesize = size;
 }
 
