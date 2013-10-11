2eb8e8a2d152a803f220850dfc7707ebfdcfa483
thelollies <rorystephenson@gmail.com>
2013-10-11 13:12:59 +1300

    fix buttons

--- js/interactions.js
+++ js/interactions.js
@@ -6,44 +6,44 @@ var conversationTexts = [ "Welcome to our town", "I'm afraid we can't offer you 
 function initInteractions(){
 	
 //Just for the buttonset thing
-				
-	$(function() {
-		$( "#radio" ).buttonset();
-	});
-	
-	$( "#radio0" ).change(function() {
-		console.log( "Handler for .click() called." );
-	});
+
+$(function() {
+	$( "#radio" ).buttonset();
+});
+
+$( "#radio0" ).change(function() {
+	console.log( "Handler for .click() called." );
+});
 			//Hide all the pop-up dialogs, for now:
-	$(function(){
-		 $("#dialogue-text").text(conversationTexts[talkTimes]);
-		 $("#dialogue-box").toggle("fold");
-	});
+			$(function(){
+				$("#dialogue-text").text(conversationTexts[talkTimes]);
+				$("#dialogue-box").toggle("fold");
+			});
 
-	$("#radio-container").toggle("fold");
+			$("#radio-container").toggle("fold");
 
-  }
-  
-function interactWith(objectType){
-	objType = objectType;
+		}
+
+		function interactWith(objectType){
+			objType = objectType;
 				//toggle = true;
-	switch(objectType){
-		case "windmill":
+				switch(objectType){
+					case "windmill":
 
-			toggleOnOrOff(true);
-			break;
-		case "house":
-			break;
-		case "car":
-			break;
-		case "nothing":
-			toggleOnOrOff(false);
-			break;
+					toggleOnOrOff(true);
+					break;
+					case "house":
+					break;
+					case "car":
+					break;
+					case "nothing":
+					toggleOnOrOff(false);
+					break;
 		case "person":  //Each person will have their own unique dialog containing...dialogue - need to look this up somewhere
-			talk();
-			break;
+		talk();
+		break;
 		default:
-			break;
+		break;
 	}
 }
 
@@ -54,106 +54,90 @@ function toggleOnOrOff(on){
 		console.log("State change");
 		if(!toggleIsOn){
 			toggleIsOn = true;}
-		else{
-			toggleIsOn = false;
-			objType = null;
-		}
-		$( "#radio-container" ).toggle( "fold" );
-	}
-}
-			
-function showMoreInfo(type){
-	if(toggleIsOn){
-		console.log("Show more info");
-		if(objType != null){
-			 console.log(infoMap[objType]);
-			 window.open(
-				  infoMap[objType], 
-				  'Information', 
-				  'width=626,height=436');
-		}
-	}
-}
-
-			function changeDisplayedSlider(dir){
-			  sliderChange(dir, "windmills");
+			else{
+				toggleIsOn = false;
+				objType = null;
 			}
+			$( "#radio-container" ).toggle( "fold" );
+		}
+	}
+
+	function showMoreInfo(type){
+		if(toggleIsOn){
+			console.log("Show more info");
+			if(objType != null){
+				console.log(infoMap[objType]);
+				window.open(
+					infoMap[objType], 
+					'Information', 
+					'width=626,height=436');
+			}
+		}
+	}
+
+	function changeDisplayedSlider(dir){
+		sliderChange(dir, "windmills");
+	}
 
 			//Change slider value
-			function sliderChange(dir, slider){ //slider is a string
+function sliderChange(dir, slider){ //slider is a string
 				if(dir == 1){ //up
 					console.log("Slider should move up");
 					if((numWindmills + 5) <= 100){
-					numWindmills += 5;
+						numWindmills += 5;
 					}
-	
+
 				}
 				else if(dir == 0){ //down
 					console.log("Slider should move down");
 					if((numWindmills - 5) >= 0){
-					  numWindmills = numWindmills - 5;}
+						numWindmills = numWindmills - 5;}
 
+					}
+					console.log("Value of numWindmills:", numWindmills);
+					if(slider == "windmills"){
+						$( "#windmills-slider" ).slider( "value", numWindmills );
+
+						$("#windmills-slider").trigger("slide");
+						$("#windmills-slider").slider("refresh");
+						$("#windmills").text("Windmills: " +numWindmills);
+					}
 				}
-    			console.log("Value of numWindmills:", numWindmills);
-			if(slider == "windmills"){
-			$( "#windmills-slider" ).slider( "value", numWindmills );
 
-			$("#windmills-slider").trigger("slide");
-			$("#windmills-slider").slider("refresh");
-			$("#windmills").text("Windmills: " +numWindmills);
-			      }
-			}
-			
-function talk(){
-	console.log("Got to talk function");
-	$("#dialogue-text").text(conversationTexts[talkTimes]);
-	talkTimes++;
-	dialogue = true;
-	$( "#dialogue-box" ).toggle( "fold" );
-}
+				function talk(){
+					console.log("Got to talk function");
+					$("#dialogue-text").text(conversationTexts[talkTimes]);
+					talkTimes++;
+					dialogue = true;
+					$( "#dialogue-box" ).toggle( "fold" );
+				}
 
 
 			/*
 				This method is fired when the user presses a number from 0 to 9. It passes that
 				number as the paramater. It is only be fired once per key press (if the number
 				is held down this will fired once only).
-			*/
+*/
 function press(number){
-	console.log(number);
-	switch(number){
-		case 0:
-			console.log("Zero");
-			$("#radio").buttonset().children("#radio0").click();
-			break;
-		case 1:
-			$("#radio").buttonset().children("#radio1").click();
-			break;
-		case 2:
-			$("#radio").buttonset().find("#radio2").click();
-			break;
-		case 3:
-			$("#radio").buttonset().find("#radio3").click();
-			break;
-		case 4:
-			$("#radio").buttonset().find("#radio4").click();
-			break;
-		default:
-			console.log("No action defined for that key");
+	
+	if (number >= 0 && number <= 4){
+		$("#radio").buttonset().children("#radio" + number).click();
 	}
+
+	$("#radio").buttonset("refresh");
 	game.effects[number].apply();
 }
 
-                        /* Shows a popup hint which is dismissable with the hide hint function
-                           (currently triggered by [enter] 
-                          */
-function showHint(x, y, text){
-              $("#hint").offset({ top: x, left: y});
-              $("#hint").text(text);
-              $("#hint").show();
-              console.log(text);
-}
-                        
-  /* Dismisses the hint popup */
-function hideHint(){
-             $("#hint").hide();
-}
+/* Shows a popup hint which is dismissable with the hide hint function
+(currently triggered by [enter] 
+	*/
+	function showHint(x, y, text){
+		$("#hint").offset({ top: x, left: y});
+		$("#hint").text(text);
+		$("#hint").show();
+	}
+
+	/* Dismisses the hint popup */
+	function hideHint(){
+		$("#hint").hide();
+	}
