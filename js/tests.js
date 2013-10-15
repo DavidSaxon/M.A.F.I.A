ad8edba97149a7cc07b5e5ea010e4d26100fe35e
thelollies <rorystephenson@gmail.com>
2013-10-15 20:08:20 +1300

    Added a base set of tests

--- js/tests.js
+++ js/tests.js
@@ -8,6 +8,27 @@ Test library specific syntax is as follows:
 */
 
 
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+test( "Front End Tests", function() {
+	
+
+	// Test whether showHint() displays a hint as expected
+	showHint(30, 40, "test hint");
+    ok($("#hint").is(":visible"), "Calling showHint() triggers the hint div" );
+    
+    // Test the hint dialogue contains the expected hint message
+    equal($("#hint").html(), "test hint", "The hint is shown with the expected content");
+    
+    // Test the hint hiding works
+    hideHint();
+    ok(!$("#hint").is(":visible"), "Test hideHint() hides the #hint div");
+
+    // Test talking dialogue functionality
+    talk();
+    ok($("#dialogue-box").is(":visible"), "Check talk() triggers talk dialogue to appear");
+
+    // Test calling talk() again makes it disappear
+    talk();
+    ok(!$("#dialogue-box").is(":visible"), "Check talk() twice hides the talk dialogue");
+
+	// More tests on the interactions...  
 });
