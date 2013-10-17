1b155c30f5365f49cb22aaca29f58641f38b9c7b
thelollies <rorystephenson@gmail.com>
2013-10-17 20:06:25 +1300

    Added more tests

--- js/tests.js
+++ js/tests.js
@@ -1,19 +1,24 @@
 /*
+
 This is the test library for this project.
 You should place a related set of tests inside a test("tests name". function(){}); function
+If functions need to be checked with delay (i.e. a slow animation is involved) use asyncTest()
 Test library specific syntax is as follows:
 	- ok(boolean, "message"); //this is for assertions
 	- equal (arg1, arg2, "message"); //tests equality on the two arguments
 
     */
 
-module("Front End Tests");
-test( "Interactions.js tests", function() {
+    // Override the toggle such that it doesn't have an animation
 
-	// Test whether showHint() displays a hint as expected
-	showHint(30, 40, "test hint");
+    module("Front End Tests");
+
+    asyncTest( "interactions.js", function() {
+
+    // Test whether showHint() displays a hint as expected
+    showHint(30, 40, "test hint");
     ok($("#hint").is(":visible"), "Calling showHint() triggers the hint div" );
-    
+
     // Test the hint dialogue contains the expected hint message
     equal($("#hint").html(), "test hint", "The hint is shown with the expected content");
     
@@ -22,17 +27,44 @@ test( "Interactions.js tests", function() {
     ok(!$("#hint").is(":visible"), "Test hideHint() hides the #hint div");
 
     // Test talking dialogue functionality
-    talk();
+    talk()
     ok($("#dialogue-box").is(":visible"), "Check talk() triggers talk dialogue to appear");
-
+   
     // A test which checks whether the dialogue goes away when dialogueOff() is called
     dialogueOff();
     ok(!dialogue, "Check talk() twice hides the talk dialogue");
-      
-	// More tests on the interactions...  
+
+    // Testing advanceText()
+    talkTimes = 0;
+    dialogue = true;
+    stopDialogue = false;
+
+    // Check advance text changes the dialogue text when appropriate
+    advanceText();
+    equal($("#dialogue-text").text(), conversationTexts[0], "Check advanceText() displays the correct message.");
+    ok(talkTimes == 1, "Check advanceText() appropriately increments talkTimes");
+
+    // Check advancing text with no messages left signals to stop dialogues
+    talkTimes = conversationTexts.length - 2;
+    advanceText();
+    ok(stopDialogue, "Check advanceText() signals to shut dialogues when there are no messages left.");
+
+    // Check advance text closes the dialogue once the last message is delivered
+    stopDialogue = true;
+    advanceText();
+    setTimeout(function() {
+    ok(!$("#dialogue-box").is(":visible"), "Check advanceText() hides #dialogue-box once all messages said.");
+    start();
+    }, 1500);
 });
 
-// To insert more tests on another file uncomment and fill the following
+// TODO 
+// test toggleOnOrOff()
+// test showMoreInfo()
+// test interactWith()
+// test other files (main.js)
+
+// To insert tests on another file uncomment and fill the following
 
 /*test( "File name", function() {
     ok(true, "dummy test name");
